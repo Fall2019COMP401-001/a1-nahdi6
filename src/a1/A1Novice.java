a6e785f8fa59de36347b08636cbe870ebffb4eb6
nahdi6 <nahdi6@152.23.117.163>
2019-08-30 19:14:12 -0400

    f

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,24 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		logic(scan);
+	}
+	public static void logic(Scanner scan) {
+		int num_customers = 0;
+		num_customers = scan.nextInt();
+		for (int i = 0; i<num_customers; i++) {
+			String fname = scan.next();
+			String lname = scan.next();
+			int items = scan.nextInt();
+			double total = 0;
+			for (int s = 0; s<items; s++) {
+				int quan = scan.nextInt();
+				String food = scan.next();
+				double price = scan.nextDouble();
+				total += price * quan; 
+				
+			}
+			System.out.print(fname.charAt(0) + ". "+ lname + ": " + total);
+		}
 	}
 }
