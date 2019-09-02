1d20ecb61fa25f537e984e22a8974b302e047cf2
nahdi6 <nahdi6@nahdi6-pc.wireless-1x.unc.edu>
2019-09-02 00:57:51 -0400

    h

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,49 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int stor = scan.nextInt();
+		String[] items = new String[stor];
+		double[] prices = new double[stor];
+		
+		for (int i=0; i<stor; i++) {
+			items[i] = scan.next();
+			prices[i] = scan.nextDouble();
+		}
+		int numCust = 0;
+		numCust = scan.nextInt();
+		String[] fname = new String[numCust];
+		String[] lname = new String[numCust];
+		double[] total = new double[numCust];
+		int g = 0;
+		int d = 0;
+		for (int s=0; s<numCust; s++) {
+			fname[s] = scan.next();
+			lname[s] = scan.next();
+			int numBought = scan.nextInt();
+			int[] numCount = new int[numBought];
+			String[] foodNames = new String[numBought];
+			for (int m=0; m<numBought; m++) {
+				numCount[m] = scan.nextInt();
+				foodNames[m] = scan.next();
+			}
+			int[] finish = new int[0];
+			for (int f=0; f<numBought; f++) {
+				while (g<items.length) {
+					if (items[g] == foodNames[f]) {
+						finish[d] = finish[g] + numCount[f]; 
+						d++;
+					} else {
+						g++;
+					}
+				}
+			
+			}
+			
+			for (int h=0; h<items.length; h++) {
+				System.out.println("0 customers bought " + finish[h] + items[h]);
+			}
+			
+		}
 		
 	}
 }
